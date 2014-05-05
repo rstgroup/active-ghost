a0f08907dc223f896d82ae247b126b1d63b67880
Wojtek Krysiak <wojciech.krysiak@kmpgroup.pl>
2014-05-05 23:52:15 +0200

    first commit

---
+++ lib/active/ghost.rb
@@ -0,0 +1,7 @@
+require "active/ghost/version"
+
+module Active
+  module Ghost
+    # Your code goes here...
+  end
+end
---
+++ lib/active/ghost/version.rb
@@ -0,0 +1,5 @@
+module Active
+  module Ghost
+    VERSION = "0.0.1"
+  end
+end
